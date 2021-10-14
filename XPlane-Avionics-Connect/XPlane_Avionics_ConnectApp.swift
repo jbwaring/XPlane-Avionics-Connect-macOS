a9fe5af1d44c52652d613ca50bbd3ba2a72a715c
jbwaring <56231422+jbwaring@users.noreply.github.com>
2021-10-13 22:12:37 -0400

    Initial Commit

---
+++ XPlane-Avionics-Connect/XPlane_Avionics_ConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  XPlane_Avionics_ConnectApp.swift
+//  XPlane-Avionics-Connect
+//
+//  Created by Jean-Baptiste Waring on 2021-10-13.
+//
+
+import SwiftUI
+
+@main
+struct XPlane_Avionics_ConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
